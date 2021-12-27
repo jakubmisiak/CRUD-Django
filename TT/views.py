5c8233564d8247aa3f26434132549457d8f65072
jakubmisiak <jakubmisiak@icloud.com>
2021-12-27 19:55:25 +0100

    readme + reqs

--- TT/views.py
+++ TT/views.py
@@ -1,4 +1,3 @@
-from apt import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
